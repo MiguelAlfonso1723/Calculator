25cd8d1f16e66d6005a9af48ae173a0d953cdd1b
Miguel Angel Alfonso <mono170203@gmail.com>
2026-04-06 23:14:11 -0500

    Creación del Display
    Se crea el display donde se van aver reflejadas las operaciones.

--- src/com/devjunior/malfonso/UI/CalculatorGUI.java
+++ src/com/devjunior/malfonso/UI/CalculatorGUI.java
@@ -1,6 +1,7 @@
 package com.devjunior.malfonso.UI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CalculatorGUI extends JFrame {
 
@@ -12,6 +13,11 @@ public class CalculatorGUI extends JFrame {
         setTitle("Calculadora");
         setSize(400,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        display = new JTextField();
+        display.setEditable(false);
+        display.setFont(new Font("Roboto", Font.PLAIN, 50));
+        add(display, BorderLayout.NORTH);
 
     }
 
