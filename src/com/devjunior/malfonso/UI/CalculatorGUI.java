ec3d0a5719be713b4e5190992264dc6c7137d713
Miguel Angel Alfonso <mono170203@gmail.com>
2026-04-07 22:53:51 -0500

    Creación de botones
    Se realizan los botones de la calculadora , estos botones incluyen los números del 0 al 9, las operaciones básicas, "=" y "." (para operaciones con decimales.

--- src/com/devjunior/malfonso/UI/CalculatorGUI.java
+++ src/com/devjunior/malfonso/UI/CalculatorGUI.java
@@ -19,6 +19,21 @@ public class CalculatorGUI extends JFrame {
         display.setFont(new Font("Roboto", Font.PLAIN, 50));
         add(display, BorderLayout.NORTH);
 
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(4,4,10,10));
+        add(panel, BorderLayout.CENTER);
+
+        String [] buttons =
+                {"7", "8", "9", "/",
+                 "4", "5", "6", "*",
+                 "3", "2", "1", "-",
+                 "0", ".", "=", "+"
+                };
+        for(String text : buttons){
+            JButton button = new JButton(text);
+            button.setFont(new Font("Roboto", Font.PLAIN, 20));
+            panel.add(button);
+        }
     }
 
 
